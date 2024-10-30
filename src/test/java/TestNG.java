9c6ef9b96f2b26f9443102c3d112139c921845cb
albert.simanjuntak <albert.simanjuntak@bytedance.com>
2024-10-30 14:23:33 +0700

    Implement restassured test1

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -1,5 +1,4 @@
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
